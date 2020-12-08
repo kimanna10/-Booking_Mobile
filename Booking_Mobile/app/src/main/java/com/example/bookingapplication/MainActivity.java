0bf6051e92ea1586a8fa4e112be4ff2ac23ebfd8
Anna Kim <55574696+kimanna10@users.noreply.github.com>
2020-12-08 15:13:17 +0600

    Add files via upload

---
+++ Booking_Mobile/app/src/main/java/com/example/bookingapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.bookingapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View backgroundImage = findViewById(R.id.background);
+        Drawable background = backgroundImage.getBackground();
+        background.setAlpha(80);
+        Button search = findViewById(R.id.search);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SearchResultsActivity.class));
+            }
+        });
+    }
+}
---
+++ Booking_Mobile/app/src/main/java/com/example/bookingapplication/SearchResultsActivity.java
@@ -0,0 +1,71 @@
+package com.example.bookingapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class SearchResultsActivity extends AppCompatActivity {
+
+    private static final String TAG = "SearchResultsActivity";
+
+    private ArrayList<String> mText = new ArrayList<>();
+    private ArrayList<Integer> mImage = new ArrayList<>();
+    ImageButton imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+
+        getImages();
+        imageView = findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SearchResultsActivity.this, SetCustomSeatActivity.class));
+            }
+        });
+
+    }
+
+    private void getImages(){
+
+        Log.d(TAG, "initImageBitMaps: preparing bitmaps.");
+        
+        mImage.add(R.drawable.bus1);
+        mText.add("Almaty-Astana");
+
+        mImage.add(R.drawable.bus2);
+        mText.add("Almaty-Astana");
+
+        mImage.add(R.drawable.bus3);
+        mText.add("Almaty-Astana");
+
+        mImage.add(R.drawable.bus4);
+        mText.add("Almaty-Astana");
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        Log.d(TAG, "initRecyclerView: init recyclerview.");
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        RecyclerView recyclerView = findViewById(R.id.listView);
+        recyclerView.setLayoutManager(layoutManager);
+        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(mText, mImage,this);
+        recyclerView.setAdapter(recyclerViewAdapter);
+
+    }
+}
